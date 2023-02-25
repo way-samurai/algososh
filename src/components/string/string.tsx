29d37cde84ce391cc12ce875ef21e7d9182acc4b
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-02-25 22:59:48 +0300

    добавил файлы стилей и JSX верстку в компонент string

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import styles from "./string.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
 
 export const StringComponent: React.FC = () => {
   return (
     <SolutionLayout title="Строка">
-     
+      <form className={styles.form} >
+        <input />
+        <Button />
+      </form>
+      <ul className={styles.list}>
+        {/* {reversArray.map((letter: string, index: number) => {
+          return (
+            <Circle
+              key={index}
+              letter={letter}
+              index={index + 1}
+              // state={stateCircle(currentIndex, index, reversArray)}
+            />)
+        })} */}
+      </ul>
     </SolutionLayout>
   );
 };
