04b8e9ae5d312ca8bf07b98c78adb6c23a12268d
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-02-27 21:16:20 +0300

    fix validation inpet values in fibbonaci component

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -3,27 +3,40 @@ import styles from "./fibonacci-page.module.css";
 import { Button } from "../ui/button/button";
 import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
-import { MAXLENGTH } from "./utils";
+import { MAXLENGTH, MINVALUE } from "./utils";
 
 export const FibonacciPage: React.FC = () => {
-  const [inputValue, setInputValue] = useState<string | number>("");
+  const [inputValue, setInputValue] = useState<string>("");
   const [isLoader, setIsLoader] = useState<boolean>(false);
   const [arrFib, setArrFib] = useState<number[]>()
+  
+  const buttonValidation = MINVALUE <= parseInt(inputValue, 10) ? false : true;
+
+  const fib = (n: number): number => {
+    if (n <= 2) {
+      return 1
+    }
+    return fib(n - 2) + fib(n-1);
+  }
 
   const onChange = (e: FormEvent<HTMLInputElement>): void => {
-    let string = e.currentTarget.value;
+    let string = e.currentTarget.value.replace(/[^0-9]/g, "");
+    console.log(string)
     const parseString = parseInt(string, 10);
     if (parseString > 19) {
       string = "19"
+    } else if (parseString <= 0) {
+      string = "1"
     }
-    setInputValue("");
+    setInputValue(string);
   };
 
   const onClick = (
     e: FormEvent<HTMLFormElement> | FormEvent<HTMLButtonElement>
   ) => {
     e.preventDefault();
-    //вызываем функцию, которая находит и отрисовывает числа фибоначи
+    const arrN = inputValue.split('');
+    console.log(arrN)
     setInputValue("");
   };
 
@@ -40,7 +53,7 @@ export const FibonacciPage: React.FC = () => {
         />
         <Button
           onClick={onClick}
-          disabled={!inputValue}
+          disabled={buttonValidation}
           isLoader={isLoader}
           text="Развернуть"
         />
