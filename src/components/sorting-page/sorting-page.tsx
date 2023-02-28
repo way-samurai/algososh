5c338dc56f967794a29dece793ec387e555a07c1
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-02-28 21:52:11 +0300

    fix styles for fibonacci component and feat styles for sorting components

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -2,19 +2,67 @@ import React, { FormEvent, useState } from "react";
 import styles from "./sorting-page.module.css";
 import { RadioInput } from "../ui/radio-input/radio-input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Button } from "../ui/button/button";
+import { Direction } from "../../types/direction";
+import { array, asc, desc, initial } from "./utils";
 
 export const SortingPage: React.FC = () => {
+  const [isLoader, setIsLoader] = useState<string>(initial);
+
   return (
     <SolutionLayout title="Сортировка массива">
       <form className={styles.form}>
-        <RadioInput label="x"/>
-        {/* <Button
-          onClick={onClick}
-          disabled={buttonValidation}
-          isLoader={isLoader}
-          text="Развернуть"
-        /> */}
+        <div className={styles.radioInputs}>
+          <RadioInput
+            //disabled={isLoader === desc || isLoader === asc}
+            label="Выбор"
+            name={"sorting-type"}
+            value={"selection-sort"}
+            defaultChecked
+            extraClass="mr-20"
+            //onChange={onChangeRadio}
+          />
+          <RadioInput
+            //disabled={isLoader === desc || isLoader === asc}
+            label="Пузырек"
+            name={"sorting-type"}
+            value={"bubble-sort"}
+            //onChange={onChangeRadio}
+          />
+        </div>
+        <div className={styles.group}>
+          <Button
+            isLoader={isLoader === asc}
+            disabled={isLoader === desc}
+            text="По возрастанию"
+            //onClick={() => onClickSort(asc)}
+            sorting={Direction.Ascending}
+            extraClass="mr-6"
+          />
+          <Button
+            isLoader={isLoader === desc}
+            disabled={isLoader === asc}
+            text="По убыванию"
+            //onClick={() => onClickSort(desc)}
+            sorting={Direction.Descending}
+            extraClass="mr-40"
+          />
+          <Button
+            disabled={isLoader !== array && isLoader !== initial}
+            text="Новый массив"
+            //onClick={onClickNewArr}
+          />
+        </div>
       </form>
+      <ul className={styles.list}>
+        {/* {randomArray.map(
+          (element: IRandomArr, index: number, state: IRandomArr[]) => {
+            return (
+              <Column key={index} index={element.num} state={element.state} />
+            );
+          }
+        )} */}
+      </ul>
     </SolutionLayout>
   );
 };
---
+++ src/components/sorting-page/utils.ts
@@ -0,0 +1,6 @@
+export const MAXLENGTH: number = 3;
+export const MINLENGTH: number = 17;
+export const asc: string = 'Direction.Ascending';
+export const desc: string = 'Direction.Descending';
+export const initial: string = 'default';
+export const array: string = 'array';
