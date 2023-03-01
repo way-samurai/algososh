a9d83b735e9c6f20a6f9992c46e48cbf562006d9
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-03-01 23:08:49 +0300

    feat: work on the sorting component has been completed

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import styles from "./sorting-page.module.css";
 import { RadioInput } from "../ui/radio-input/radio-input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
@@ -21,7 +21,7 @@ import { Column } from "../ui/column/column";
 import { TRandomArr } from "../../types/sorting-page";
 import { ElementStates } from "../../types/element-states";
 import { pause } from "../../utils";
-import { DELAY_IN_MS } from "../../constants/delays";
+import { DELAY_IN_MS, SHORT_DELAY_IN_MS } from "../../constants/delays";
 
 export const SortingPage: React.FC = () => {
   const [isLoader, setIsLoader] = useState<string>(initial);
@@ -46,27 +46,58 @@ export const SortingPage: React.FC = () => {
       for (let j = i + 1; j < length; j++) {
         array[j].state = ElementStates.Changing;
         setRandomArr([...array]);
-        await pause(DELAY_IN_MS);
-        if (directionAsc ? array[j].num < array[currentInd].num : array[j].num > array[currentInd].num) {
+        await pause(SHORT_DELAY_IN_MS);
+        if (
+          directionAsc
+            ? array[j].num < array[currentInd].num
+            : array[j].num > array[currentInd].num
+        ) {
           currentInd = j;
-          array[currentInd].state = ElementStates.Default
+          array[currentInd].state = ElementStates.Default;
         }
         if (j !== currentInd) {
-          array[j].state = ElementStates.Default
+          array[j].state = ElementStates.Default;
         }
-        setRandomArr([...array])
+        setRandomArr([...array]);
       }
       swap(array, i, currentInd);
       array[currentInd].state = ElementStates.Default;
       array[i].state = ElementStates.Modified;
       setRandomArr([...array]);
     }
-    setIsLoader(initial)
-    return array
+    setIsLoader(initial);
+    return array;
+  };
+
+  const bubbleSort = async (
+    array: TRandomArr[],
+    direction: string
+  ): Promise<TRandomArr[]> => {
+    const { length } = array;
+    const directionAsc = direction === Direction.Ascending ? true : false;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        const left = array[j].num;
+        const right = array[j + 1].num;
+        array[j].state = ElementStates.Changing;
+        array[j + 1].state = ElementStates.Changing;
+        setRandomArr([...array]);
+        await pause(DELAY_IN_MS);
+        if (directionAsc ? left > right : left < right) {
+          array[j].num = right;
+          array[j + 1].num = left;
+        }
+        array[j].state = ElementStates.Default;
+        setRandomArr([...array]);
+      }
+      array[array.length - i - 1].state = ElementStates.Modified;
+      setRandomArr([...array]);
+    }
+    setIsLoader(initial);
+    return array;
   };
 
   const onChangeRadio = (e: ChangeEvent<HTMLInputElement>): void => {
-    e.preventDefault();
     setSortingMethod((e.target as HTMLInputElement).value);
   };
 
@@ -77,17 +108,17 @@ export const SortingPage: React.FC = () => {
   const onClickSortArr = async (direction: string): Promise<void> => {
     setIsLoader(direction);
     if (sortingMethod === radioSelectionSort) {
-      setRandomArr([... await selectionSort(randomArr, direction)])
+      setRandomArr([...(await selectionSort(randomArr, direction))]);
     } else if (sortingMethod === radioBubbleSort) {
-      //setRandomArr(... await )
+      setRandomArr([...(await bubbleSort(randomArr, direction))]);
     }
     return;
   };
 
   useEffect(() => {
-    renderRandomArr()
-  }, [])
-  
+    renderRandomArr();
+    return () => setRandomArr([])
+  }, []);
 
   return (
     <SolutionLayout title="Сортировка массива">
@@ -100,14 +131,14 @@ export const SortingPage: React.FC = () => {
             value={radioSelectionSort}
             defaultChecked
             extraClass="mr-20"
-            onChange={() => onChangeRadio}
+            onChange={onChangeRadio}
           />
           <RadioInput
             disabled={isLoader === desc || isLoader === asc}
             label="Пузырек"
             name={"sorting-type"}
             value={radioBubbleSort}
-            onChange={() => onChangeRadio}
+            onChange={onChangeRadio}
           />
         </div>
         <div className={styles.group}>
--- src/constants/delays.ts
+++ src/constants/delays.ts
@@ -1,2 +1,2 @@
-export const DELAY_IN_MS = 500;
-export const SHORT_DELAY_IN_MS = 300;
+export const DELAY_IN_MS = 800;
+export const SHORT_DELAY_IN_MS = 500;
