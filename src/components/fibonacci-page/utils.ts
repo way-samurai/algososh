6b68f37f7fb26d51fd309c6ef371d281a24bdd5b
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-03-04 21:04:20 +0300

    --amend

--- src/components/fibonacci-page/utils.ts
+++ src/components/fibonacci-page/utils.ts
@@ -1,6 +1,3 @@
-export const MAXLENGTH: number = 2;
-export const MINVALUE: number = 1;
-
 export const getFibArray = (stringValue: string) => {
   const n = parseInt(stringValue, 10);
   const fibArr = [1, 1];
--- src/components/sorting-page/utils.ts
+++ src/components/sorting-page/utils.ts
@@ -1,18 +1,5 @@
-import { Direction } from "../../types/direction";
 import { ElementStates } from "../../types/element-states";
 import { TRandomArr } from "../../types/sorting-page";
-
-export const MAXLENGTH: number = 3;
-export const MINLENGTH: number = 17;
-export const MAXVALUE: number = 100;
-
-export const asc: string = Direction.Ascending;
-export const desc: string = Direction.Descending;
-export const initial: string = 'default';
-export const array: string = 'array';
-
-export const radioSelectionSort: string = 'selection-sort';
-export const radioBubbleSort: string = 'bubble-sort';
 
 export const generateRandomArray = (
     MAXLENGTH: number,
