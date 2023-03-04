cc3b991c63adeb2249608ac616816e580e2e2664
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-03-04 21:07:47 +0300

    feat: types for sorting-page component

---
+++ src/types/stack.ts
@@ -0,0 +1,17 @@
+import { ElementStates } from './element-states';
+
+export interface IStack<T> {
+  push: (item: T) => void;
+  pop: () => void;
+  clear: () => void;
+  peak: () => T | null;
+  getElements: () => T[];
+  getSize: () => number;
+}
+
+export type TStackElement = {
+  value: string;
+  state: ElementStates;
+};
+
+export type TProcess = { isAdding: boolean; isRemoving: boolean };
