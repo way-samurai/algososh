5cc75b0d69af29697e0decb8c0acac917f8759e6
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-03-30 01:17:06 +0300

    refactoring tests for string component

--- src/components/string/__tests__/string.test.js
+++ src/components/string/__tests__/string.test.js
@@ -1,19 +1,19 @@
-import { reverseString } from "../utils";
+import { getReversingStringSteps } from "../utils";
 
 describe("Тестирование компонента String", () => {
   it("Корректно разворачивает строку с четным количеством символов", () => {
-    expect(reverseString("1ABC23")).toEqual(["3", "2", "C", "B", "A", "1"]);
+    expect(getReversingStringSteps("1ABC23")).toEqual(["3", "2", "C", "B", "A", "1"]);
   });
 
   it("Корректно разворачивает строку с нечетным количеством символов", () => {
-    expect(reverseString("1ABC2")).toEqual(["2", "C", "B", "A", "1"]);
+    expect(getReversingStringSteps("12345")).toEqual([["5", "4", "3", "2", "11"]]);
   });
 
   it("Корректно разворачивает строку с одним символом", () => {
-    expect(reverseString("1ABC2")).toEqual(["2", "C", "B", "A", "1"]);
+    expect(getReversingStringSteps("1234")).toEqual([["4", "3", "2", "1"]]);
   });
 
   it("Корректно разворачивает пустую строку", () => {
-    expect(reverseString("")).toEqual([]);
+    expect(getReversingStringSteps("")).toEqual([[]]);
   });
 });
