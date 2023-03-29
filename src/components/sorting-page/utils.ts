921352cb9131d840e2ba221ee1920a08d09fa600
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-03-30 01:26:25 +0300

    --amend

--- src/components/sorting-page/utils.ts
+++ src/components/sorting-page/utils.ts
@@ -2,23 +2,67 @@ import { ElementStates } from "../../types/element-states";
 import { TRandomArr } from "../../types/sorting";
 
 export const generateRandomArray = (
-    MAXLENGTH: number,
-    MINLENGTH: number,
-    MAXVALUE: number
-  ) => {
-    const arrLength = Math.floor(
-      Math.random() * (MAXLENGTH - MINLENGTH) + MINLENGTH
-    );
-    const arr = [...new Array(arrLength)].map(() =>
-      Math.round(Math.random() * MAXVALUE)
-    );
-    const arrColumns = arr.map((num) => ({ num, state: ElementStates.Default }));
+  MAXLENGTH: number,
+  MINLENGTH: number,
+  MAXVALUE: number
+) => {
+  const arrLength = Math.floor(
+    Math.random() * (MAXLENGTH - MINLENGTH) + MINLENGTH
+  );
+  const arr = [...new Array(arrLength)].map(() =>
+    Math.round(Math.random() * MAXVALUE)
+  );
+  const arrColumns = arr.map((num) => ({ num, state: ElementStates.Default }));
 
-    return arrColumns
-  };
+  return arrColumns;
+};
 
-export const swap = (arr: TRandomArr[], firstIndex: number, secondIndex: number): void => {
-	const temp = arr[firstIndex];
-	arr[firstIndex] = arr[secondIndex];
-	arr[secondIndex] = temp;
+export const swap = (
+  arr: TRandomArr[],
+  firstIndex: number,
+  secondIndex: number
+): void => {
+  const temp = arr[firstIndex];
+  arr[firstIndex] = arr[secondIndex];
+  arr[secondIndex] = temp;
+};
+
+export const selectionSort = (
+  array: TRandomArr[],
+  directionAsc: boolean
+): TRandomArr[] => {
+  const { length } = array;
+  for (let i = 0; i < length; i++) {
+    let currentInd = i;
+
+    for (let j = i + 1; j < length; j++) {
+      if (
+        directionAsc
+          ? array[j].num < array[currentInd].num
+          : array[j].num > array[currentInd].num
+      ) {
+        currentInd = j;
+      }
+    }
+    swap(array, i, currentInd);
+  }
+  return array;
+};
+
+export const bubbleSort = (
+  array: TRandomArr[],
+  directionAsc: string
+): TRandomArr[] => {
+  const { length } = array;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length - i - 1; j++) {
+      const left = array[j].num;
+      const right = array[j + 1].num;
+      if (directionAsc ? left > right : left < right) {
+        array[j].num = right;
+        array[j + 1].num = left;
+      }
+    }
+  }
+  return array;
 };
--- src/components/string/__tests__/string.test.js
+++ src/components/string/__tests__/string.test.js
@@ -1,16 +1,28 @@
 import { getReversingStringSteps } from "../utils";
 
+const resultForEvenSymbols = [
+  ["1", "2", "3", "4"],
+  ["4", "2", "3", "1"],
+  ["4", "3", "2", "1"],
+];
+
+const resultForOddSymbols = [
+  ["1", "2", "3", "4", "5"],
+  ["5", "2", "3", "4", "1"],
+  ["5", "4", "3", "2", "1"],
+];
+
 describe("Тестирование компонента String", () => {
   it("Корректно разворачивает строку с четным количеством символов", () => {
-    expect(getReversingStringSteps("1ABC23")).toEqual(["3", "2", "C", "B", "A", "1"]);
+    expect(getReversingStringSteps("1234")).toEqual(resultForEvenSymbols);
   });
 
   it("Корректно разворачивает строку с нечетным количеством символов", () => {
-    expect(getReversingStringSteps("12345")).toEqual([["5", "4", "3", "2", "11"]]);
+    expect(getReversingStringSteps("12345")).toEqual(resultForOddSymbols);
   });
 
   it("Корректно разворачивает строку с одним символом", () => {
-    expect(getReversingStringSteps("1234")).toEqual([["4", "3", "2", "1"]]);
+    expect(getReversingStringSteps("1")).toEqual([["1"]]);
   });
 
   it("Корректно разворачивает пустую строку", () => {
